ce3e89409c2a0697a31134bd10d7d4e84072e0b5
Rakshitha Sathyakumar <57928306+rakshitha-sathyakumar@users.noreply.github.com>
2021-03-03 09:28:55 -0800

    Update the mongo DB string

--- kafka-backend/config.js
+++ kafka-backend/config.js
@@ -1,5 +1,5 @@
 const config = {
-    mongoDB: 'mongodb+srv://admin:password1234@yelpapp.se5yk.mongodb.net/yelpDb?retryWrites=true&w=majority'
+    mongoDB: 'mongoDB_URL'
 };
 
 module.exports = config;
