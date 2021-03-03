64c858e67243ed2114c06bc3073a686dcea95051
Rakshitha Sathyakumar <57928306+rakshitha-sathyakumar@users.noreply.github.com>
2021-03-03 09:19:25 -0800

    updated the mongoDB url

--- Backend/config.js
+++ Backend/config.js
@@ -1,5 +1,5 @@
 const config = {
-    mongoDB: 'mongodb+srv://admin:password1234@yelpapp.se5yk.mongodb.net/yelpDb?retryWrites=true&w=majority'
+    mongoDB: 'mongodb://[username:password@]host1[:port1][,...hostN[:portN]][/[defaultauthdb][?options]]'
 };
 
 module.exports = config;
