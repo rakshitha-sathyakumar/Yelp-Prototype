9aad08c8a6e1a1a6c172dbe503ff246795f6e611
Rakshitha Sathyakumar <57928306+rakshitha-sathyakumar@users.noreply.github.com>
2021-03-03 09:24:56 -0800

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,7 @@ app.use(cors({ origin: 'http://localhost:3000', credentials: true }));
 
 //use express session to maintain session data
 app.use(session({
-    secret: 'cmpe273_kafka_passport_mongo',
+    secret: 'ADD YOUR SECRET KEY',
     resave: false, // Forces the session to be saved back to the session store, even if the session was never modified during the request
     saveUninitialized: false, // Force to save uninitialized session to db. A session is uninitialized when it is new but not modified.
     duration: 60 * 60 * 1000,    // Overall duration of Session : 30 minutes : 1800 seconds
